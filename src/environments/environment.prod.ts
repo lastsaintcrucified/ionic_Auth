81130bf1834cc4f00327c7ebb54c676795404e3e
lastsaintcrucified <lastsaintcrucified@gmail.com>
2021-08-02 09:52:30 +0600

    first ionic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDMoENxRL7ZC1mh4-tDzjRgP1_TU7vNXNE',
+    authDomain: 'ionicauth-8087f.firebaseapp.com',
+    projectId: 'ionicauth-8087f',
+    storageBucket: 'ionicauth-8087f.appspot.com',
+    messagingSenderId: '386311497552',
+    appId: '1:386311497552:web:002fe36e8534b0abf12e55',
+    measurementId: 'G-VTHKQZXPQD',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDMoENxRL7ZC1mh4-tDzjRgP1_TU7vNXNE',
+    authDomain: 'ionicauth-8087f.firebaseapp.com',
+    projectId: 'ionicauth-8087f',
+    storageBucket: 'ionicauth-8087f.appspot.com',
+    messagingSenderId: '386311497552',
+    appId: '1:386311497552:web:002fe36e8534b0abf12e55',
+    measurementId: 'G-VTHKQZXPQD',
+  },
 };
 
 /*
